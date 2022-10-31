cecf0e847ddeada4e66b343cbb34d0612f9add3c
temagor <temagor2015@gmail.com>
2022-10-31 17:56:35 +0300

    fix car migration

--- database/migrations/2022_10_31_140354_create_cars_table.php
+++ database/migrations/2022_10_31_140354_create_cars_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
 
             $table->foreignIdFor(User::class)
                 ->nullable()
+                ->unique()
                 ->references('id')
                 ->on('users')
                 ->nullOnDelete();
